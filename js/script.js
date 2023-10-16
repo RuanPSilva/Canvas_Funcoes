654f0b1ffa7dedc439f82a90603869e31814cbd2
KauanJesusJD <kauan.santos76@etec.sp.gov.br>
2023-10-16 12:15:33 -0300

    Arrumando o tamanho da tela do Canvas e sua cor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');//Linkando o canvas com o HTML
-	 ctx.canvas.width=window.innerWidth;
-     ctx.canvas.height=window.innerHeight;
+	 ctx.canvas.width=1400;
+     ctx.canvas.height=600;
 	//Inicio do Barco
 	 ctx.beginPath();
 	 ctx.fillStyle = '#8B4513';
